036e045dec554e0e5f03beb1498f608f752706b2
inx <inx_glb@qq.com>
2021-03-14 17:19:16 +0800

    科技Hub

--- B2/sijishe.js
+++ B2/sijishe.js
@@ -1,19 +1,19 @@
 // ==UserScript==
-// @name              科技HUB
+// @name              司机社
 // @namespace         https://soulsign.inu1255.cn?account=yeoncc
 // @version           1.0.0
 // @author            yeoncc
-// @loginURL          https://kejihub.com
-// @updateURL         https://soulsign.inu1255.cn/script/yeoncc/科技HUB
+// @loginURL          https://sijishe.org/
+// @updateURL         https://soulsign.inu1255.cn/script/yeoncc/司机社
 // @expire            1000000
-// @domain            www.kejihub.com
+// @domain            sijishe.org
 // @param            Authorization 密钥
 // ==/UserScript==
 
-exports.run = async function(param) {
-if (!param.Authorization) throw '密匙为空，请先设置密匙'
+exports.run = async function (param) {
+  if (!param.Authorization) throw '密匙为空，请先设置密匙'
 
-  let { data, status } = await axios.post('https://www.kejihub.com/wp-json/b2/v1/getUserMission', 'count=10&paged=1', {
+  let { data, status } = await axios.post('https://sijishe.org/wp-json/b2/v1/getUserMission', 'count=10&paged=1', {
     headers: {
       'Authorization': param.Authorization
     }
@@ -25,7 +25,7 @@ if (!param.Authorization) throw '密匙为空，请先设置密匙'
     return '已签到，今日获取积分：' + data.mission.credit + ' ,总积分：' + data.mission.my_credit
   }
 
-  axios.post('https://www.kejihub.com/wp-json/b2/v1/userMission', '', {
+  axios.post('https://sijishe.org/wp-json/b2/v1/userMission', '', {
     headers: {
       'Authorization': param.Authorization
     }
@@ -37,10 +37,10 @@ if (!param.Authorization) throw '密匙为空，请先设置密匙'
   })
 };
 
-exports.check = async function(param) {
-if (!param.Authorization) throw '密匙为空，请先设置密匙'
+exports.check = async function (param) {
+  if (!param.Authorization) throw '密匙为空，请先设置密匙'
 
-  let { data, status } = await axios.post('https://www.kejihub.com/wp-json/b2/v1/getUserMission', 'count=10&paged=1', {
+  let { data, status } = await axios.post('https://sijishe.org/wp-json/b2/v1/getUserMission', 'count=10&paged=1', {
     headers: {
       'Authorization': param.Authorization
     }
